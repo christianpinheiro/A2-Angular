af85dd206bea71c9e799b984d7debd2a4b4b6f06
christianpinheiro <93pinheiro@gmail.com>
2022-06-17 18:08:29 -0300

    4 files modified

--- src/app/christian-tasks-service.service.ts
+++ src/app/christian-tasks-service.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@angular/core';
 
+interface Tarefas {
+  t: string;
+}
+
 @Injectable()
 export class ChristianTasksServiceService {
+  t: string;
+  list: Array<Tarefas> = [];
+  meuStorage = localStorage;
+  cont = 0;
 
-  constructor() { }
+  add(t: string) {
+    this.list.push({ t });
+    this.meuStorage.setItem('list', this.t);
+    this.cont++;
+  }
 
+  remove(index: number) {
+    this.list.splice(index, 1);
+    this.meuStorage.removeItem('list');
+    this.cont--;
+  }
+
+  getList() {
+    return this.list;
+  }
+
+  constructor() {}
 }
