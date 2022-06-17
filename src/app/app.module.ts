3a5ff8e1eaf76e542990fa4a30be3cfcf6669627
christianpinheiro <93pinheiro@gmail.com>
2022-06-17 17:37:29 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,25 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { RouterModule } from '@angular/router';
+import { ChristianHomeComponentComponent } from './christian-home-component/christian-home-component.component';
+import { ChristianTasksComponentComponent } from './christian-tasks-component/christian-tasks-component.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ChristianHomeComponentComponent },
+      { path: 'tarefas', component: ChristianTasksComponentComponent },
+    ]),
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    ChristianHomeComponentComponent,
+    ChristianTasksComponentComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
