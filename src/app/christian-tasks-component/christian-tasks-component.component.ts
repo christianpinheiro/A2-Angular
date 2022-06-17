eadafcc1198c5aea76d660c707e0c753b69b0a67
christianpinheiro <93pinheiro@gmail.com>
2022-06-17 17:33:54 -0300

    1 file modified, 4 added

---
+++ src/app/christian-tasks-component/christian-tasks-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-christian-tasks-component',
+  templateUrl: './christian-tasks-component.component.html',
+  styleUrls: ['./christian-tasks-component.component.css']
+})
+export class ChristianTasksComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
